f32770c309fd8656a559cd73b61a05ed66ae8926
vedantcoder <nichalvedant07@gmail.com>
2025-06-28 14:29:08 +0530

    Final Commit before deploy

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  darkMode: 'class', // enables class-based dark mode toggle
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // adjust paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
